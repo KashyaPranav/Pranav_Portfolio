cec4edf3942499f57aecc01b24e937dc1561c624
Pranav Kumar Kashyap <pranavkumarkashyap@Pranavs-MacBook-Air.local>
2026-01-11 06:12:17 +0530

    final

--- src/components/MobileHome.tsx
+++ src/components/MobileHome.tsx
@@ -46,7 +46,7 @@ export default function MobileHome() {
         <div className="text-xl font-bold text-zinc-200">Pranav Kumar Kashyap</div>
         <div className="text-sm text-zinc-400 mb-2 italic">Software Engineer</div>
         <a
-          href="https://drive.google.com/file/d/1y5Jkm59Vb_Sq54yTkm3xDh5yElaJmwOq/view?usp=drivesdk"
+          href="https://drive.google.com/file/d/1eMZ8ohhLLICC5lZyUqAeR8_u2AGn3IGH/view?usp=drivesdk"
           target="_blank"
           rel="noopener noreferrer"
           className="bg-zinc-900 text-white px-6 py-2 rounded-md text-xs font-semibold hover:bg-zinc-700 transition"
@@ -132,7 +132,7 @@ export default function MobileHome() {
           </a>
         </div>
         <a
-          href="https://drive.google.com/file/d/1y5Jkm59Vb_Sq54yTkm3xDh5yElaJmwOq/view?usp=drivesdk"
+          href="https://drive.google.com/file/d/1eMZ8ohhLLICC5lZyUqAeR8_u2AGn3IGH/view?usp=drivesdk"
           target="_blank"
           rel="noopener noreferrer"
           className="text-white px-4 py-2 rounded-md text-xs font-semibold transition border border-zinc-700"
